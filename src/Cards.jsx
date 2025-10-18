aca62a1732958c22aae070447e312b9f234e411c
Golam <74001687+golam71@users.noreply.github.com>
2025-10-18 14:12:30 +0600

    🔥 Finish Game

---
+++ src/Cards.jsx
@@ -0,0 +1,63 @@
+import "./css/cards.css";
+import shuffleArray from "./shuffle";
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function Cards({ count, setCount }) {
+  const [catData, setCatData] = useState([]);
+  const [clicked, setClicked] = useState([]);
+
+  useEffect(() => {
+    async function getCatImageUrls() {
+      const img = await fetch(
+        "https://api.thecatapi.com/v1/images/search?limit=12&size=med&mime_types=jpg&order=RANDOM&api_key=live_4xcACum8Qf3UMhr8O4Dw9kd0m8mush3bifrpN3Bk5ndOoHdJO7u7jRmvjuEsIgMD"
+      );
+      const data = await img.json();
+      setCatData(data);
+    }
+
+    getCatImageUrls();
+  }, []);
+
+  function handleClick(e) {
+    const src = e.target.src;
+    setCatData(shuffleArray(catData));
+
+    if (clicked.includes(src)) {
+      alert(`Your score is ${count}`);
+      setCount(0);
+      setClicked([]);
+      return;
+    }
+
+    const updatedClicked = [...clicked, src];
+    setClicked(updatedClicked);
+    setCount(count + 1);
+
+    if (updatedClicked.length === 12) {
+      alert("You Won the game! Click ok to play again :)");
+      setClicked([]);
+      setCount(0);
+    }
+  }
+
+  return (
+    <div className="grid">
+      <AnimatePresence>
+        {catData.map((data) => (
+          <motion.img
+            key={data.url}
+            src={data.url}
+            alt=""
+            height="300px"
+            width="300px"
+            className="img"
+            onClick={handleClick}
+            layout
+            transition={{ duration: 0.4 }}
+          />
+        ))}
+      </AnimatePresence>
+    </div>
+  );
+}
---
+++ src/TextContent.jsx
@@ -0,0 +1,13 @@
+export default function TextContent({ count }) {
+  return (
+    <>
+      <div className="flex">
+        <h2>Memory Game</h2>
+        <p>
+          score : <span>{count}</span>
+        </p>
+      </div>
+      <p>Click images to get points but dont click an image twice.</p>
+    </>
+  );
+}
